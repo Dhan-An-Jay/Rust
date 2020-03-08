6b234c63ba9e239fe74b1273746f04771112e216
DhanAnJay Ahire <dhananjay@DhanAnJays-MacBook.local>
2020-03-08 22:39:09 +0530

    Ownership basic assignment

---
+++ ownership_assignment/target/debug/deps/ownership_assignment-5664c93e31faaa10.d
@@ -0,0 +1,5 @@
+/Users/dhananjay/Documents/Rust/ownership_assignment/target/debug/deps/ownership_assignment-5664c93e31faaa10: src/main.rs
+
+/Users/dhananjay/Documents/Rust/ownership_assignment/target/debug/deps/ownership_assignment-5664c93e31faaa10.d: src/main.rs
+
+src/main.rs:
---
+++ ownership_assignment/target/debug/ownership_assignment.d
@@ -0,0 +1,1 @@
+/Users/dhananjay/Documents/Rust/ownership_assignment/target/debug/ownership_assignment: /Users/dhananjay/Documents/Rust/ownership_assignment/src/main.rs
